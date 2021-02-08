8935309e38465b6bc9ef9ea642e3c691550f5850
morpheuslord <70637311+morpheuslord@users.noreply.github.com>
2021-02-08 15:10:17 +0530

    Add files via upload

---
+++ config/list.cmd
@@ -0,0 +1,77 @@
+@echo off
+echo.
+	echo.
+	call colorbox.exe /0d 25 9 39 16
+	call colorbox.exe /0b 34 14 48 21
+	call colorbox.exe /0c 34 4 48 11
+	call colorbox.exe /0d 43 9 57 16
+	call colorbox.exe /0b 26 10 40 17
+	call colorbox.exe /0c 35 15 49 22
+	call colorbox.exe /0d 35 5 49 12
+	call colorbox.exe /0b 44 10 58 17
+	call colorbox.exe /0c 27 11 41 18
+	call colorbox.exe /0d 36 16 50 23
+	call colorbox.exe /0b 36 6 50 13
+	call colorbox.exe /0c 45 11 59 18
+	echo.
+	echo.
+	echo.
+	echo.
+	echo.
+	echo.
+	echo.
+	echo.
+	echo.
+	echo.
+	echo.
+	echo.
+	echo.
+	echo.
+	echo.
+	echo.
+	echo.
+	echo.
+	echo.
+	echo.
+	echo  [-------------------------------------------------------------------------------------------------------------------------------]
+	ECHO.
+	call colorchar.exe /0D " 	                              THE BRUTE-FRAMEWORK                                                         "
+	ECHO.
+	echo  [-------------------------------------------------------------------------------------------------------------------------------]
+	echo.
+
+echo.
+call colorchar.exe /0B " LISTAING OF ALL TOOLS "	
+echo.
+echo  - atom
+echo  - brute-framework
+echo  - portabletools
+echo  - photon
+echo  - virtualbox
+echo  - cygwin
+echo  - terminus
+echo  - curl 
+echo  - nikto
+echo  - joomscan
+echo  - dotdotpwn
+echo  - padbuster
+echo  - recon-ng
+echo  - recon-web
+echo  - instaosint
+echo  - creepy
+echo  - ngrep
+echo  - fast-brute
+echo  - evil-winrm
+echo  - ettercap
+echo  - ruby
+echo  - python27
+echo  - python3
+echo  - perl
+echo  - gem
+echo  - pip3
+echo  - choco
+echo  - duf
+echo  - pip2
+echo  - urh
+echo.
+PAUSE
